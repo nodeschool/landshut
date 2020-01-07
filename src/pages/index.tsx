69ef4907c3ca264901e969365a6b7bf69a7a5275
Daniel Weiß <wanieldeiss@gmail.com>
2020-01-07 05:18:01 +0100

    remove link to death page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import { Link } from "gatsby";
 
 import Layout from "../components/layout";
-import Image from "../components/image";
 import SEO from "../components/seo";
-import Header from "../components/header";
 import Community from "../components/community";
 import Mentors from "../components/mentors";
 import NodeSchoolChapterLogo from "../components/NodeschoolChapterSection";
@@ -15,7 +13,6 @@ const IndexPage = () => (
     <NodeSchoolChapterLogo></NodeSchoolChapterLogo>
     <Community></Community>
     <Mentors></Mentors>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 );
 
